2bc8a1eb235df3fc2ebdcf64165d278810f9d8ed
johng <jcguest90@gmail.com>
2020-11-17 13:06:07 -0600

    write file to memory, run FFmpeg command, read result, create a URL

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
 
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
 
+  // initialize ffmpeg and setReady
   const load = async () => {
     await ffmpeg.load();
     setReady(true);
@@ -19,7 +21,23 @@ function App() {
     load();
   }, [])
 
+  const convertToGif = async () => {
+  // write file to memory
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+  // run the FFmpeg command
+  await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+  // read the result
+  const data = ffmpeg.FS('readFile', 'out.gif');
+
+  // create a URL
+  const url = URL.createObjectURL(new Blob([data.buffer]));
+
+  }
+
   return ready ? (
+    // display video only when defined, convert video to URL for browser
     <div className= "App">
       { video && <video
                       controls
